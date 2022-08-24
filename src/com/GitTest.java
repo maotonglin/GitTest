405d9e252e459fce60b5859616da60673e701a5e
lin <lin@glodon.com>
2022-08-24 21:40:10 +0800

    hot-fix-1

--- src/com/GitTest.java
+++ src/com/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("1.....");
         System.out.println("2.....");
         System.out.println("3.....");
+        System.out.println("4.....");
+        System.out.println("5.....");
+        System.out.println("6.....");
     }
 }
