92349e6d40d11f09b87b32e73190b0c503eceb8c
lin <lin@glodon.com>
2022-08-26 19:05:41 +0800

    feature-1

--- src/com/GitTest.java
+++ src/com/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("5.....");
         System.out.println("6.....");
         System.out.println("feature.7.....");
+        System.out.println("feature.8.....");
     }
 }
